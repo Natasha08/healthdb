e547383de86d78778b7cddbcc5f6f5839081e855
Tulsi <natashaosborne@otstudy.com>
2016-03-24 10:03:51 -0600

    Files added from oldgreekgod website

---
+++ nutrition.php
@@ -0,0 +1,83 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kinobuild</title>
+    <link rel="stylesheet" href="css/normalize.css">
+   <link href='http://fonts.googleapis.com/css?family=Marck+Script%7COpen+Sans:400italic,700italic,800italic,700,800,400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body class="meat">
+    <header id="top" class="main-header2">
+      <h1 class="subhead">For the Greek Warrior</h1>
+      <img class="arrow" src="img/arrow.svg" alt="Down arrow">
+    </header>
+    <div class="primary-content t-border">
+      <nav>
+       <ul>
+         <li><a href="index.html">Logout</a></li>
+       </ul>
+      </nav>
+      <nav>
+       <ul>
+        <li><a href="input.htm">Input</a></li>
+        <li><a href="reports.htm">Reports</a></li>
+        <li><a href="nutrition_database.htm">Nutrition</a></li>
+      </ul>
+      </nav>
+
+<?php
+ 
+ if ( $_POST)
+{
+
+require '../connect_db.php';
+
+if (!$dbcon) {
+     die('Connection Failed');
+} else {
+     echo "<br><br><br>Great Job!<br>";
+ }    
+
+    
+$food_name= $_POST['food_name'];
+$food_brand= $_POST['food_brand'];
+$serving_amount= $_POST['serving_amount'];
+$s_unit= $_POST['s_unit'];
+$serving_size= $_POST['serving_size'];
+$total_calories= $_POST['total_calories'];
+$fat_grams= $_POST['fat_grams'];
+$carbohydrate_grams= $_POST['carbohydrate_grams'];
+$protein_grams= $_POST['protein_grams'];
+$total_grams= $_POST['total_grams'];
+$fat_ratio= $_POST['fat_ratio'];
+$carbohydrate_ratio= $_POST['carbohydrate_ratio'];
+$protein_ratio= $_POST['protein_ratio'];
+
+
+$fdata = "INSERT INTO otstudyc_nwork.nutrition (food_name, food_brand, serving_amount, s_unit, serving_size, total_calories, fat_grams, carbohydrate_grams, protein_grams, total_grams, fat_ratio, carbohydrate_ratio, protein_ratio) VALUES ('$food_name', '$food_brand', '$serving_amount', '$s_unit','$serving_size', '$total_calories', '$fat_grams', '$carbohydrate_grams', '$protein_grams', '$total_grams', '$fat_ratio', '$carbohydrate_ratio', '$protein_ratio')"; 
+
+if (mysqli_query($dbcon, $fdata)) {
+
+    echo "<br><br<br>Your new food has been saved!";
+     
+} else {
+    
+    die("Error submitting form.");
+}
+
+  
+mysqli_close($dbcon); 
+
+ }
+
+?>
+</div>
+  <div class="main-footer2">
+      <footer>
+       <a href="http://twitter.com/natasha_marie_o"><img src="img/twitter-wrap.png" alt="Twitter Logo" class="social-icon"></a>
+        <a href="http://facebook.com/greek_esposa"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"></a>
+        <p>&copy; 2015-2016 Natasha Osborne.</p>
+      </footer>
+  </div>
+</body>
+</html>
---
+++ nutritiontable.php
@@ -0,0 +1,63 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kinobuild</title>
+    <link rel="stylesheet" href="css/normalize.css">
+   <link href='http://fonts.googleapis.com/css?family=Marck+Script%7COpen+Sans:400italic,700italic,800italic,700,800,400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body class="meat">
+    <header id="top" class="main-header2">
+      <h1 class="subhead">For the Greek Warrior</h1>
+      <img class="arrow" src="img/arrow.svg" alt="Down arrow">
+    </header>
+   
+      <nav>
+       <ul>
+         <span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+         <li><a href="index.html">Logout</a></li>
+       </ul>
+      </nav>
+      <nav>
+       <ul>
+       <span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> 
+        <li><a href="input.htm">Input</a></li>
+        <li><a href="reports.htm">Reports</a></li>
+        <li><a href="nutrition.htm">Nutrition</a></li>
+      </ul>
+     </nav>
+     
+<?php
+
+require '../connect_db.php';
+
+// Check connection
+if (!$dbcon) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected". "<br>". "<br>";
+}
+
+ 
+   $sql= "SELECT * FROM otstudyc_nwork.nutrition";
+   
+  $result = mysqli_query($dbcon, $sql); 
+  While ($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)): {
+ 
+ ?>
+
+<br><br><br>
+<form action="n_input.php", method="post"> 
+<tr class="nrow">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label for="n_box"><?php echo $rows['food_name']; ?></label>&nbsp;&nbsp;&nbsp;  
+  <input type="checkbox" value="<?php echo $rows['food_name']; ?>"></input>
+</tr>
+</table>
+<?php }endwhile; ?>
+  <input type="submit" name="Submit Calories"></input>
+</form>
+
+
+</body>
+
+</html>
