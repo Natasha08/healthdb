c2490584c6f905a532cf8dbbb79f753c85996e0d
Tulsi <natashaosborne@otstudy.com>
2016-03-31 15:57:54 -0600

    remove event listener from login panel edit

--- js/panel.js
+++ js/panel.js
@@ -4,8 +4,9 @@ var lgbttn = document.getElementById('login')
 function panelAppear() {	
 	lgpanel.setAttribute('class', 'loginpanel');
 	lgbttn.removeAttribute('hidden');
+	lgpanel.removeEventListener(panelAppear, true);
 }
 
 lgpanel.addEventListener('mouseover', panelAppear, false);
-lgpanel.removeEventListener(panelAppear, true);
+
 
