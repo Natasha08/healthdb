cebb0f3fd0fc594d825e183599f0de8c7c9186f5
Tulsi <natashaosborne@otstudy.com>
2016-03-31 13:41:59 -0600

    presentation and markup edits

--- js/blur.js
+++ js/blur.js
@@ -1,26 +1,9 @@
-/**target the login button by using getElementByID and the id is login
-wrap it also in variable, or wrap the entire thing in an anonymous function**/
+
+// bit of code changes the CSS class of the 'login' button to no longer blur
 
 var lgbutton = document.getElementById('blurs');
-
-//if (lgbutton.length > 0) {
 
 lgbutton.setAttribute('class', 'nonblur');
 
 
-
-//} else {
-
-//	alert('Didn\'t work');
-//}
-//lgbutton.removeAttribute('class');
-
-
-
-
-//remove the blur class when the page loads? Or some event.
-
-
-/**The statement above assumes that #login is inheriting the blur class from
-its parent**/
 
