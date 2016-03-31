f548e18b3254aef3c016ab5ca1ef825297607b4a
Tulsi <natashaosborne@otstudy.com>
2016-03-31 15:41:38 -0600

    eds.

--- js/panel.js
+++ js/panel.js
@@ -1,9 +1,11 @@
 var lgpanel = document.getElementById('loginpanel');
+var lgbttn = document.getElementById('login')
 
 function panelAppear() {	
 	lgpanel.setAttribute('class', 'loginpanel');
+	lgbttn.removeAttribute('hidden');
 }
 
 lgpanel.addEventListener('mouseover', panelAppear, false);
-lgpanel.removeEventListener();
+lgpanel.removeEventListener(panelAppear, true);
 
