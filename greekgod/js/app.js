34a3d785c08fc20e205296016d423da25bfb4c4f
Tulsiqt <natashaosborne@otstudy.com>
2016-04-12 08:42:11 -0600

    updating working

--- greekgod/js/app.js
+++
@@ -1,4 +0,0 @@
-var x=754;
-var y=569;
-
-console.log(x+y);
--- greekgod/js/blur.js
+++
@@ -1,6 +0,0 @@
-
-// bit of code changes the CSS class of the 'login' button to no longer blur
-
-var lgbutton = document.getElementById('blurs');
-
-lgbutton.setAttribute('class', 'nonblur');
